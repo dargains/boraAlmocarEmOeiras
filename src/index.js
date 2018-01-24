d313c12e5cbe2d306580b74788273fbda6054870
Andre Dargains <andre.dargains@gmail.com>
2018-01-24 19:14:16 +0000

    ADD: firebase;

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyDjEf0thjCR7FKg_NIabIrNjggb0_5NwPs",
+    authDomain: "bora-almocar-em-oeiras.firebaseapp.com",
+    databaseURL: "https://bora-almocar-em-oeiras.firebaseio.com",
+    projectId: "bora-almocar-em-oeiras",
+    storageBucket: "bora-almocar-em-oeiras.appspot.com",
+    messagingSenderId: "1061964015752"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
