ef3b53245e7a28f2afe13f640a46fde743516206
Andre Dargains <andre.dargains@gmail.com>
2018-01-31 19:13:26 +0000

    UPD: body overflow;

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -43,7 +43,6 @@ class Form extends Component {
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="4">4</option>
-              <option value="5">5</option>
             </select>
           </label>
 
